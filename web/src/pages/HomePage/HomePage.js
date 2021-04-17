7dffc4cd1e79deb540d2381c0af26f710b2edc74
Isaac Tait <isaac@mountaintopcoding.com>
2021-04-17 09:56:21 -0700

    update home page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,17 +5,38 @@ const HomePage = () => {
     <div>
       <div className="flex flex-wrap align-content-center">
         <h1 className="font-semibold text-red-500">
-          Welcome to my portfolio site!
+          Welcome to the new and improved mountainTopCoding(
+          <span role="img" aria-label="mountain with snow-cap">
+            &#127956;
+          </span>
+          );!
         </h1>
-
+      </div>
+      <div>
         <p>
-          Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+          Find the original{' '}
+          <code>
+            mountainTopCoding(
+            <span role="img" aria-label="mountain with snow-cap">
+              &#127956;
+            </span>
+            );
+          </code>
+          <a
+            href="https://mountaintopcoding.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-red-500 hover:bg-red-500 hover:text-white"
+          >
+            here
+          </a>
         </p>
       </div>
       <>
+        <p>This is ultra basic for now, more to come soon.</p>
         <p>
-          My default route is named <code>home</code>, link to me with `
-          <Link to={routes.home()}>Home</Link>`
+          Here is a link to `<Link to={routes.home()}>Home</Link>`, which you
+          are already at so it will appear not to work...
         </p>
       </>
     </div>
