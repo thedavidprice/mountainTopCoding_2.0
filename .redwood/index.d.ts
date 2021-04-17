36866476ffcff262d8a0025238128d54fc63e525
Isaac Tait <isaac@mountaintopcoding.com>
2021-04-17 09:19:37 -0700

    why does this file keep updating itself?

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2021-04-17T16:02:31.265Z
+// 2021-04-17T16:17:58.285Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
