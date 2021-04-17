bbfeac54fe0fe0aacefe0623cfcea8f9654e5987
Isaac Tait <isaac@mountaintopcoding.com>
2021-04-17 09:45:52 -0700

    why does this not work!?

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,16 +2,23 @@ import { Link, routes } from '@redwoodjs/router'
 
 const HomePage = () => {
   return (
-    <>
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </>
+    <div>
+      <div className="flex flex-wrap align-content-center">
+        <h1 className="font-semibold text-red-500">
+          Welcome to my portfolio site!
+        </h1>
+
+        <p>
+          Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+        </p>
+      </div>
+      <>
+        <p>
+          My default route is named <code>home</code>, link to me with `
+          <Link to={routes.home()}>Home</Link>`
+        </p>
+      </>
+    </div>
   )
 }
 
