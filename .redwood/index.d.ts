585b78f6e4f5e05f618e89b8c105d22ec0e526c8
Isaac Tait <isaac@mountaintopcoding.com>
2021-04-24 17:45:18 -0700

    change title of page and add favicon

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2021-04-24T23:34:27.359Z
+// 2021-04-25T00:39:07.681Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
