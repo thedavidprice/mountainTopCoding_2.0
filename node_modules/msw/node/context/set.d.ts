7f6af473bc0793b0e2f21c2fc0b569c32a57cdc7
Isaac Tait <isaac@mountaintopcoding.com>
2021-05-13 19:35:44 -0700

    taking canary for a spin

--- node_modules/msw/node/context/set.d.ts
+++
@@ -1,2 +0,0 @@
-import { ResponseTransformer } from '../response';
-export declare function set<N extends string | Record<string, string | string[]>>(...args: N extends string ? [N, string] : [N]): ResponseTransformer;
--- node_modules/msw/node/context/status.d.ts
+++
@@ -1,2 +0,0 @@
-import { ResponseTransformer } from '../response';
-export declare const status: (statusCode: number, statusText?: string | undefined) => ResponseTransformer;
