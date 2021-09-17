406dca1eb55941d178eee853a2134d231594ee82
David Price <thedavid@thedavidprice.com>
2021-09-16 23:58:39 -0700

    update .gitignore; cleanup git cache; update package.json files

--- .redwood/index.d.ts
+++
@@ -1,4 +0,0 @@
-// 2021-05-24T03:11:20.720Z
-/// <reference path="./types/import-dir-schemas.d.ts" />
-/// <reference path="./types/import-dir-services.d.ts" />
-/// <reference path="./types/routes.d.ts" />
